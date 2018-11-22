7304da41700925c778e7cb780872c94716b212ce
SkyeN <skye@dames.es>
2018-11-21 23:11:35 -0800

    add date query string to GET endpoint

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-import express from 'express';
-import db from './db/db';
+import express from "express";
+import db from "./db/db";
+
+let todayDate = new Date();
 
 const app = express();
-
-app.get('/api/investments',(req,res)=>{
-  res.status(200).send({
-    success:'true',
-    message: 'portfolio retrieved successfully',
-    portfolios: db
-  })
+app.get("/carta/investments", (req, res) => {
+  if (!req.query.date || new Date(req.query.date) > todayDate) {
+    res.status(200).send({
+      success: "true",
+      message: "portfolio retrieved successfully",
+      date: todayDate, //don't need to show this
+      portfolios: db
+    });
+  } else {
+    res.status(200).send({
+      success: "true",
+      message: "portfolio retrieved successfully",
+      date: req.query.date, //don't need to show this
+      portfolios: db
+    });
+  }
 });
 
 const PORT = 5000;
 
-app.listen(PORT,()=>{
-  console.log(`server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
