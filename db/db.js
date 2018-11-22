85d11cf50a11f76f80ef7cd2ac1ef80b723721c8
SkyeN <skye@dames.es>
2018-11-21 23:35:30 -0800

    modify respone structure to include dates

--- db/db.js
+++ db/db.js
@@ -1,14 +1,46 @@
-const portfolios =  [
-    {
-      "company": "Meetly",
-      "quantity": 1000,
-      "cost": 1000
+const portfolios = [
+  {
+    company: "Meetly",
+    quantity: {
+      current: 1000,
+      history: [
+        {
+          "2017-11-12": 200,
+          "2018-04-13": 1000
+        }
+      ]
     },
-    {
-      "company": "IMIM",
-      "quantity": 1000,
-      "cost": 1000
+    cost: {
+      current: 1500,
+      history: [
+        {
+          "2017-07-14": 400,
+          "2018-04-13": 1500
+        }
+      ]
     }
+  },
+  {
+    company: "IMIM",
+    quantity: {
+      current: 1000,
+      history: [
+        {
+          "2017-11-12": 300,
+          "2018-04-13": 1000
+        }
+      ]
+    },
+    cost: {
+      current: 1200,
+      history: [
+        {
+          "2017-07-14": 300,
+          "2018-04-13": 1200
+        }
+      ]
+    }
+  }
 ];
 
 export default portfolios;
