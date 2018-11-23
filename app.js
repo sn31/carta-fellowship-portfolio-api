3d082830664a942d62ad3c962548aad51d960a1d
SkyeN <skye@dames.es>
2018-11-23 11:23:07 -0800

    refactor getTransactions method

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 import express from "express";
 import db from "./db/db";
-import mysql from 'mysql';
-import databaseInfo from './database';
+import mysql from "mysql";
+import databaseInfo from "./database";
 
 //Connect to database
 let connect = mysql.createPool(databaseInfo);
 
-let read_R = 'SELECT*FROM transactions';
-
-connect.getConnection(function(err,connection) {
-  connection.query(read_R, function(err,data){
-      if (err) console.log(err);
+function getTransations() {
+  let read_R = "SELECT*FROM transactions";
+  connect.getConnection(function(err, connection) {
+    connection.query(read_R, function(err, data) {
+      if (err) throw err;
       else {
-          console.log(data);
+        console.log(data);
       }
+    });
+    connection.release();
   });
-  connection.release();
-});
-
-
+}
 
 let todayDate = new Date();
 
@@ -33,6 +32,7 @@ app.get("/carta/investments", (req, res) => {
       portfolios: db
     });
   } else {
+    getTransations();
     res.status(200).send({
       success: "true",
       message: "portfolio retrieved successfully",
